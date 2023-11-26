3271bdeb01ead2ffdb98d6a6165ceaa33ca12c32
Burenkov Oleg <duncanscoffee@gmail.com>
2023-11-26 12:31:51 +0200

    initaial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13676; // стоимость билета
+
+        int bonus = amount / 20; // Начисляемые мили
+
+        System.out.println("Итоговый бонус = " + bonus);
+    }
+}
